98c701f61c49540a321e6c9ad4e7b3c121cfd846
Santiago Castrillon <santiago123@gmail.com>
2021-04-26 13:22:42 -0300

    Iniciando o projeto

---
+++ Meu_projeto/meu_app/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.db import models
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    slug =  models.SlugField(max_length=255, unique=True)
+    
