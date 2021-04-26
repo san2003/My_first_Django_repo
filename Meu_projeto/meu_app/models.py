79408f94586be5e439ed13ee44a03569a5e70ea6
Santiago Castrillon <santiago123@gmail.com>
2021-04-26 13:40:52 -0300

    comit test

--- Meu_projeto/meu_app/models.py
+++ Meu_projeto/meu_app/models.py
@@ -4,5 +4,5 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    slug =  models.SlugField(max_length=255, unique=True)
+    slug =  models.SlugField(max_length=254, unique=True)
     
