97853f20d83ddbc8ef662772968fd4b7ee0fd3a1
Santiago Castrillon <santiago123@gmail.com>
2021-04-26 14:53:23 -0300

    ajustando parametrros django

--- Meu_projeto/meu_app/models.py
+++ Meu_projeto/meu_app/models.py
@@ -1,8 +1,12 @@
 from django.db import models
-from django.db import models
-# Create your models here.
+from django.contrib.auth.models import User
+#Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
-    slug =  models.SlugField(max_length=254, unique=True)
-    
+    slug =  models.SlugField(max_length=255, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
